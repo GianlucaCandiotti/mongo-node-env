1520a0f5cf3343ab2a720b2c803e9639e14e7e58
Gianluca Candiotti <gianluca.prwlr@gmail.com>
2015-05-22 15:58:56 -0500

    Added mongo manual installation default.pp

--- vagrant-env/puppet/manifests/default.pp
+++ vagrant-env/puppet/manifests/default.pp
@@ -22,4 +22,23 @@ package { $sysPackages:
  
 class { 'nodejs':
   version => 'stable',
+  before => Exec['import-mongo-key'],
+}
+
+exec { 'import-mongo-key':
+  command => 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10',
+  timeout => 0,
+} ->
+exec { 'list-file-mongo':
+  command => 'echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list',
+  timeout => 0,
+  notify => Exec['apt_update'],
+  before => Exec['install-mongo'],
+}
+
+exec { 'install-mongo':
+  command => 'sudo apt-get install -y mongodb-org',
+  timeout => 0,
+  tries => 3,
+  onlyif => "sudo apt-get update"
 }
